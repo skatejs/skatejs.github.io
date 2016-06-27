5e1a7a37641529323e2096e6efc880bc95a00f8b
Trey Shugart <treshugart@gmail.com>
2016-06-27 21:09:53 +1000

    chore(build): Initial dist and rollup config for jsnext:main.

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global.skatejsGithubIo = global.skatejsGithubIo || {})));
+}(this, function (exports) {
+
+	Object.defineProperty(exports, '__esModule', { value: true });
+
+}));
+//# sourceMappingURL=index.js.map
