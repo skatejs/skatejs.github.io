d258b0b4c99bc0e7ff4001e9cc1b705cfc98302d
Trey Shugart <treshugart@gmail.com>
2016-06-30 21:15:57 +1000

    chore: Remove React support from the babel transpiler as it was messing up JSX for Incremental DOM.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
 const conf = module.exports = require('skatejs-build/webpack.config');
 conf.devtool = 'source-map';
 conf.module.loaders[0].loader = 'css?modules&camelCase';
+conf.module.loaders[2].query.presets = [];
