570f54e006fcc996163a25c5f62812930973c48d
Joshua Nelson <jonelson@atlassian.com>
2016-10-08 17:41:47 +1100

    fix(header): fix css header in Chrome

--- src/pages/docs/index.js
+++ src/pages/docs/index.js
@@ -5,7 +5,7 @@ export default define('sk-page-docs', {
   render() {
     return (
       <Layout>
-        <p>The docs are currently being ported over from the <a href="https://github.com/skatejs/skatejs">README</a>.</p>
+        <p>The docs are currently being ported over from the <a href="https://github.com/skatejs/skatejs/blob/master/README.md">README</a>.</p>
       </Layout>
     );
   },
